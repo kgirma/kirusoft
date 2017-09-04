8aa95363d7449e825cde46a7264dac1cd9d266b1
Kirubel G <Kiruswagz@gmail.com>
2017-09-04 19:19:27 -0400

    added icon

--- Projects/kirusoft/ios/kirusoftTests/kirusoftTests.m
+++ Projects/kirusoft/ios/kirusoftTests/kirusoftTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface kirusoftTests : XCTestCase
+@interface KirusoftTests : XCTestCase
 
 @end
 
-@implementation kirusoftTests
+@implementation KirusoftTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
